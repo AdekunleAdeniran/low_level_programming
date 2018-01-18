1074899e05788075be67efb4b39f8c012c09d932
flourishcodes <kunle@adeniran.com>
2018-01-18 17:29:15 +0000

    Learning to write() in a new way

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * main - main block
  * Return: 1
  */
-int main(void) 
+int main(void)
 {
 int len;
-len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19", len);
+len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
 return (1);
 }
