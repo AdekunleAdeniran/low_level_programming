73038c11a91a74cb7385f95201b2ed2bf41fd567
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 12:56:35 -0800

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,18 +5,18 @@
  * main - Entry Point
  * Program checks if int n is zero, positive or negative
  * Return: 0
- */
+*/
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	        printf("%d is positive\n", n);
+	else if (n == 0)
+	        printf("%d is zero\n", n);
+	else 
+	        printf("%d is negative\n", n);
+	return (0);
 }
