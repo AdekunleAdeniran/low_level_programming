0ea7157d1b2a0d1fa0bceea708c1bcd8503932ae
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 16:42:01 -0800

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet with each letter on a new line
+ * using putchar only twice
+ * Return: 0
+ */
+int main(void)
+{
+        char alpha = 'a';
+        while (alpha < 'z')
+        {
+        putchar(alpha);
+        alpha++;
+        putchar('\n');
+        }
+        return (0);
+}
