c6a8e893bb155f087b6305dc4c0abbfab6aff92c
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 20:09:56 -0800

    Create 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print numbers from 0 to 99.
+ * Numbers must be separated by commas followed by a space.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 5 times.
+ * You are not allowed to use variables of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+  int b;
+  int c;
+
+	for (a = 0; a < 100; a++)
+	{
+		b = a / 10;
+		c = a % 10;
+
+		putchar(b + '0');
+		putchar(c + '0');
+
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
