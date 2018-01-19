1806919c963bd3865b7d21e1162687b6173f1442
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 19:29:02 -0800

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,21 @@
  * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
- int main(void)
- {
- int a;
- for (a = 0; a < 10; a++)
- {
- putchar( a + '0');
- if (a < 9)
- {
- putchar(44);
- putchar(32);
- }
- }
- putchar('\n');
- return (0);
- }
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
