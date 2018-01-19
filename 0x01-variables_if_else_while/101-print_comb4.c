429b68d58fa45864396c4c3d40eb5d9d7cbbc4c8
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 23:17:42 -0800

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,30 +14,26 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c;
-	int d;
+	int a, b, c, d;
 
 	for (a = 0; a < 1000; a++)
 	{
-		b = a / 100;
-		c = (a / 10) % 10;
-		d = a % 10;
-                
-		if ( b < c && c < d)
-                {
-		putchar(b + '0');
-		putchar(c + '0');
-                putchar (d +'0');
-                
-                
-		if (a < 999)
+		b = a / 100; /* hundreds */
+		c = (a / 10) % 10; /* tens */
+		d = a % 10; /* singles */
+
+		if (b < c && c < d)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (a < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-                }
 	}
 	putchar('\n');
 
