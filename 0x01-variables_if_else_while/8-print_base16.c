8fecef235b28f9b742ad25fa572b569258e07f7b
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 19:03:56 -0800

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-        int a;
-        
-        for (a = 0; a < 10; a++)
-        {
-                putchar('0' + a);
-        }
-        for (a = 'a'; a <= 'f'; a++)
-        {
-                putchar(a);
-        }
-        putchar('\n');
-        
-        return (0);
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
 }
