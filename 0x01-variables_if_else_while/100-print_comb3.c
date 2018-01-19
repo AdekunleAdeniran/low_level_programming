b2bbe27bda7ba734881d43490e1c1450cea9d216
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 22:39:13 -0800

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,26 +13,30 @@
  */
 int main(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a < 100; a++)
-{
-b = a % 10; /* singles digit */
-c = a / 10; /* doubles digit */
-if (b < c)
-{
-putchar(b + '0');
-putchar(c + '0');
-        
-if (a < 99)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
+	int a, b, c;
 
-return (0);
+	a = 0;
+
+	while (a < 100)
+	{
+		b = a % 10; /* singles digit */
+		c = a / 10; /* doubles digit */
+
+		if b < c)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+
+			if (c < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
 }
