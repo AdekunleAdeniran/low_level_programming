c2e7ddd9a38a484f6ceb6ec398d01d4498a662fd
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 18:08:12 -0800

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+int a =0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("\n");
+return (0);
+}
