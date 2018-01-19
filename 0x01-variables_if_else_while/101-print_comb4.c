ba80d46347ac47c6f8e7560373047b50476d88ee
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 23:13:44 -0800

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits.
+ * Numbers must be separated by commas and a space.
+ * The 3 digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * print only the smallest combination of 3 digits.
+ * Numbers should be printed in ascending order.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 6 times.
+ * You are not allowed to use any variables of type `char`.
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 1000; a++)
+	{
+		b = a / 100;
+		c = (a / 10) % 10;
+		d = a % 10;
+                
+		if ( b < c && c < d)
+                {
+		putchar(b + '0');
+		putchar(c + '0');
+                putchar (d +'0');
+                
+                
+		if (a < 999)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+                }
+	}
+	putchar('\n');
+
+	return (0);
+}
