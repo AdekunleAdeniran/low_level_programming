04756fca1a3fa8927bab6d3d3922ec36430a9655
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-01-18 17:02:26 -0800

    Rename 2-print_alphabet.c to 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print the alphabet on a line omitting e and q
+ * using putchar only twice
+ * Return: 0
+ */
+int main(void)
+{
+char alpha = 'a';
+while (alpha < 'z')
+{
+putchar(alpha);
+alpha++;
+if (alpha == 'e' || alpha == 'q')
+alpha++;
+}
+putchar('\n');
+return (0);
+}
