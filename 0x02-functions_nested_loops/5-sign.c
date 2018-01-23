c3b46b5da5519c2429f2e60318bc84aab48240af
flourishcodes <kunle@adeniran.com>
2018-01-23 21:37:28 +0000

    Check if number is positive or negative or zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_sign - check if n is +, - or 0
+ *
+ * @n: int type
+ *
+ * Return: 1 if n is > 0, 0 is zero and -1 if < 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
