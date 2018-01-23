eb9fa0799b59bde85a10c1da04a56f62c944c959
flourishcodes <kunle@adeniran.com>
2018-01-23 17:50:48 +0000

    NO library functions allowed

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,8 +2,10 @@
 
 /**
  * main - Entry Point.use only user defined variable to create output.
+ *
  * Description: Print Holberton using predefined functions
- * Return: Always success
+ *
+ * Return: 0
  */
 
 int main(void)
@@ -16,5 +18,6 @@ int main(void)
 		_putchar(h[i]);
 	}
 	_putchar('\n');
+
 	return (0);
 }
