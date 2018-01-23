b9878882781f74a675428582939ad247f8f3e5c0
flourishcodes <kunle@adeniran.com>
2018-01-23 22:33:10 +0000

    Check for absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ * @a: int type
+ *
+ * Return: return absolute value of integer
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (a * -1);
+	else
+		return (a);
+}
