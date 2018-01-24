107569d56422167b5b970e037a4573f3fb0234bb
flourishcodes <kunle@adeniran.com>
2018-01-24 02:07:46 +0000

    jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,44 +9,18 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = '0'; a <= '1'; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = 0; b <= 59; b++)
 		{
-			for (c = '0'; c <= '5'; c++)
-			{
-				for (d = '0'; d <= '9'; d++)
-				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-			}
-		}
-	}
-	int e, f, g, h;
-
-	for (e = '2'; e < '3'; e++)
-	{
-		for (f = '0'; f <= '3'; f++)
-		{
-			for (g = '0'; g <= '5'; g++)
-			{
-				for (h = '0'; h <= '9'; h++)
-				{
-					_putchar(e);
-					_putchar(f);
-					_putchar(':');
-					_putchar(g);
-					_putchar(h);
-					_putchar('\n');
-				}
-			}
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
