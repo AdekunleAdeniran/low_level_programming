2da0c97de4eaa323fe0269e355b583fbbe02026e
flourishcodes <kunle@adeniran.com>
2018-01-24 01:16:33 +0000

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include "holberton.h"
+
+/**
+ * add - get the last digit
+ *
+ * @a: int type
+ * @b: int type
+ *
+ * Return: sum of two intergers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
