2a5c3a1490f4c4d24de4bace79281ba95b6808a0
flourishcodes <kunle@adeniran.com>
2018-01-23 23:52:09 -0800

    check the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - selects the last digit of an integer
+ * @n: int type
+ *
+ * Return: the absolute value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+
+	if (n >= 0)
+		m = n % 10;
+	else
+		m = (n % 10) * -1;
+	_putchar(m + '0');
+	return (m);
+}
