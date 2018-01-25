f687dcf5a7c86e9529505e651255b5022ccf29e1
flourishcodes <kunle@adeniran.com>
2018-01-24 16:33:29 -0800

    Times table for multiple input

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+/**
+ * print_times_table - takes an input and prints out the times
+ * @n: int type
+ * Return: return table of values
+ */
+void print_times_table(int n)
+{
+	int a, b;
+
+	if (n > 0 && n < 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				if (b == 0)
+					_putchar((a * b) + '0');
+				else if ((a * b) <= 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((a * b) + '0');
+			}
+				else if ((a * b) >= 10 && (a * b) <= 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(((a * b) / 10) + '0');
+				_putchar(((a * b) % 10) + '0');
+			}
+				else if ((a * b) > 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(((a * b) / 100) + '0');
+				_putchar((((a * b) / 10) % 10) + '0');
+				_putchar(((a * b) % 10) + '0');
+			}
+		}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
