7b5ce91753aa1c80113a711dbccdba7c69a9934f
flourishcodes <kunle@adeniran.com>
2018-01-24 21:19:48 -0800

    Add  the multiple results

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/**
+ * main - Program to sum integers
+ *
+ * Description: check for all natural numbers in range of
+ * 3 and 5 and sum them
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 1024; a++)
+	{
+
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
