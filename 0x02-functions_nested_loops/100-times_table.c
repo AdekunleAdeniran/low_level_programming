49ee9b7427e48bc4fc2985e7bbf5db2869c07fe5
flourishcodes <kunle@adeniran.com>
2018-01-24 17:10:02 -0800

    Times table for multiple input

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,25 +6,25 @@
  */
 void print_times_table(int n)
 {
+	if (n < 0 || n > 15)
+		return;
 	int a, b;
 
-	if (n > 0 && n < 15)
+	for (a = 0; a <= n; a++)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			for (b = 0; b <= n; b++)
+			if (b == 0)
+				_putchar((a * b) + '0');
+			else if ((a * b) <= 9)
 			{
-				if (b == 0)
-					_putchar((a * b) + '0');
-				else if ((a * b) <= 9)
-				{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((a * b) + '0');
 			}
-				else if ((a * b) >= 10 && (a * b) <= 99)
+			else if ((a * b) >= 10 && (a * b) <= 99)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -32,7 +32,7 @@ void print_times_table(int n)
 				_putchar(((a * b) / 10) + '0');
 				_putchar(((a * b) % 10) + '0');
 			}
-				else if ((a * b) > 99)
+			else if ((a * b) > 99)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -41,9 +41,6 @@ void print_times_table(int n)
 				_putchar(((a * b) % 10) + '0');
 			}
 		}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
-	else
-		return;
 }
