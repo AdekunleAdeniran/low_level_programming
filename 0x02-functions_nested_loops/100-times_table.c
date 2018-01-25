18390b25565b90bf7887101bd9b31d1f2b188e5f
flourishcodes <kunle@adeniran.com>
2018-01-24 17:22:11 -0800

    Times table for multiple input

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,12 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
 	int a, b;
 
+	if (n < 0 || n > 15)
+		return;
+	else
+{
 	for (a = 0; a <= n; a++)
 	{
 		for (b = 0; b <= n; b++)
@@ -18,6 +20,7 @@ void print_times_table(int n)
 				_putchar((a * b) + '0');
 			else if ((a * b) <= 9)
 			{
+
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
@@ -43,4 +46,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
+}
 }
