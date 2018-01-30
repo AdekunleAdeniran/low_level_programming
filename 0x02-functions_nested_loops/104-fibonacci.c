f62bb125e87807ccd9e6911465a043d943efafa2
flourishcodes <kunle@adeniran.com>
2018-01-29 18:04:40 -0800

    fibonacci  98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 number
+ *
+ * Description: fibobnachi numbers starting from one
+ * Return: 0
+ */
+
+int main(void)
+{
+	double fib1  = 0;
+	double fib2  = 1;
+	double fib3;
+	int counter = 1;
+
+	while (counter <= 98)
+	{
+		fib3 =  fib2 + fib1;
+		counter++;
+	printf("%.0f", fib3);
+		if (counter <= 98)
+			printf(", ");
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	printf("\n");
+	return (0);
+}
