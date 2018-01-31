3013a2336ad7f7b73f16f4bbe22ae98b9828849d
flourishcodes <kunle@adeniran.com>
2018-01-30 21:40:47 -0800

    print half of string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts_half - returns half of string
+ * @str: type char
+ *
+ * Return: half of string
+ */
+
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+
+	}
+	for (a /= 2; str[a] > 0; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
