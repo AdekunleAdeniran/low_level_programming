b9ca73803d10d2405b03cfc833b48cfaa1e41354
flourishcodes <kunle@adeniran.com>
2018-01-30 18:22:03 -0800

    reverse a string

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+
+int _strlen(char *s)
+{
+	int a, count;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		count = 0;
+		count += a;
+	}
+	return (count);
+}
+
+/**
+ * rev_string - reverse given string
+ *
+ * @s: char type
+ * Return: a string in reverse
+ */
+
+void rev_string(char *s)
+{
+	int a, length;
+	char holder;
+
+	a = 0;
+	length = _strlen(s);
+	while (length >= a)
+	{
+		holder = s[a];
+		s[a] = s[length];
+		s[length] = holder;
+		length--;
+		a++;
+	}
+}
