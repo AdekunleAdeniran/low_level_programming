041f389cfda6b1b7f21bacead685b428ed944a5d
flourishcodes <kunle@adeniran.com>
2018-02-01 22:53:50 -0800

    reverse an array

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse an array of int
+ * @a: pointer to an array
+ * @n: int type for size of array
+ * Return: reversed array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0, j = n - 1; i <= n / 2; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
