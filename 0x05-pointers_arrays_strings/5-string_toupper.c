94aaaa6eec63a91a62a9817e5d8997ef03a213dd
flourishcodes <kunle@adeniran.com>
2018-02-02 16:53:26 +0000

    change string to uppercase

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,22 @@
 #include "holberton.h"
 
+/**
+ * string_toupper - change a string to upper case
+ * @s: pointer to a string
+ * Return: s
+ */
+
 char *string_toupper(char *s)
 {
 	int i = 0;
-	while(s[i]!='\0')
+
+	while (s[i] != '\0')
 	{
-		if(s[i]>='a' && s[i]<='z'){
-			s[i]=s[i]-32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
 		}
 		++i;
 	}
+	return (s);
 }
