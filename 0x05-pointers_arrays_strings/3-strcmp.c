8fda56b25d11f778537599bd3f64051ef4ba5075
flourishcodes <kunle@adeniran.com>
2018-02-01 22:22:21 -0800

    string compare

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,19 +7,15 @@
  * @s2: char type
  * Return: positive negative or zero
  */
+int a;
 
-int _strcmp(char *s1, char *s2)
+for (a = 0; s1[a] != '\0'; a++)
+	;
+for (a = 0; s2[a] != '\0'; a++)
 {
-	int a;
-
-	for (a = 0; s1[a] != '\0'; a++)
-		;
-	for (a = 0; s2[b] != '\0'; a++)
+	if (s1[a] != s2[a])
 	{
-		if (s1[a] != s2[a])
-		{
-			return (s1[a] - s2[a]);
-		}
+		return (s1[a] - s2[a]);
 	}
-	return (0);
 }
+return (0);
