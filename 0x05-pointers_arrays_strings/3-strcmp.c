86e2ab265a1f1e3df8754aed8d02a671d070a995
flourishcodes <kunle@adeniran.com>
2018-02-01 22:15:27 -0800

    string compare

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
- */
-
-int _strlen(char *s)
-{
-	int b;
-
-	for (b = 0; s[b] != '\0'; b++)
-	{
-
-	}
-	return (b);
-}
-/**
  * _strcmp - return length of a string
  *
  * @s1: char type
@@ -27,10 +10,16 @@ int _strlen(char *s)
 
 int _strcmp(char *s1, char *s2)
 {
-	if (_strlen(s1) == _strlen(s2))
-		return (0);
-	else if (_strlen(s1) > _strlen(s2))
-		return (15);
-	else
-		return (-15);
+	int a;
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+	for (a = 0; s2[b] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+	}
+	return (0);
 }
