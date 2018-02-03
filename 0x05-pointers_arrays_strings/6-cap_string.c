509aaf7b6b9a67f973790f1bdd3329e8a2f55015
flourishcodes <kunle@adeniran.com>
2018-02-02 17:07:13 -0800

    make words to uppercase

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+char *cap_string(char *s)
+{
+	int a, b;
+
+	char spec[13]  = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+			  '(', ')', '{', '}'};
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; spec[b] != '\0'; b++)
+		{
+			if (a == 0)
+			{
+				if (s[a]  >= 'a' && s[a] <= 'z')
+				{
+					s[a] = s[a] - 32;
+				}
+			}
+			if (s[a] == spec[b])
+			{
+				if (s[a + 1] >= 'a' && s[a + 1] <= 'z')
+				{
+					s[a + 1] = s[a + 1] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
