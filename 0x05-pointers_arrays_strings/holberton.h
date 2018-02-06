fe1e15ccf854c714f9fe9cdb4ddc257f12b6f40e
flourishcodes <kunle@adeniran.com>
2018-02-05 23:43:23 -0800

    updates

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -17,3 +17,9 @@ char *leet(char *);
 char *rot13(char *);
 
 int _strlen(char *s);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
