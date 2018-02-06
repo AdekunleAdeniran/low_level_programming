90a1afb5ba93fab89a88ebc03d6a9cd4b03d65ea
flourishcodes <kunle@adeniran.com>
2018-02-05 23:06:19 -0800

    sum diagonal in array

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums- sum diagonal values
+ * @a: pointer to array of int type
+ * @size: int type
+ * Return: sum of two sides
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int x, y, suma, sumb;
+
+	suma = 0;
+	sumb = 0;
+
+	for (x = 0;  x < size; x++)
+		suma += a[(size + 1) * x];
+	for (y =  0; y < size; y++)
+		sumb += a[(size - 1) * (y + 1)];
+	printf("%d, %d\n",  suma, sumb);
+}
