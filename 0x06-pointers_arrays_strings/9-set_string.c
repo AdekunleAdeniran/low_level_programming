bcd8ca198de644a045db7b26dec799a1b3d02ffb
flourishcodes <kunle@adeniran.com>
2018-02-05 23:23:08 -0800

    Pointer to pointer

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string
+ * @s: pointer to pointer of string
+ * @to: pointer to string
+ * Return: always success
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+        *s = to;
+}
