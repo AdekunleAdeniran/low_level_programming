e0db78dd3a403f2548b1200fdc6cb9490805f284
flourishcodes <kunle@adeniran.com>
2018-02-07 23:23:22 +0000

    factorial

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * factorial - find factorial of numbers;
+ * @n: int type
+ * Return: returns -1 if number less than zero, else return factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n -1));
+}
