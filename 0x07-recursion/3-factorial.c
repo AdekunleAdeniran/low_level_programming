7a09713a69e0a1b8f0ca8b3e6df1bcd0b1125169
flourishcodes <kunle@adeniran.com>
2018-02-07 17:24:17 -0800

    corrections to factorial

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -8,13 +8,10 @@
 
 int factorial(int n)
 {
-	if (n == 0)
-	{
-		return (1);
-	}
 	if (n < 0)
-	{
 		return (-1);
-	}
-	return (n * factorial(n -1));
+	if (n >= 1)
+		return (n * factorial(n - 1));
+	else
+		return (1);
 }
