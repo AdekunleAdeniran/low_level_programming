809f825a205411c69a9bcf55a428335a58d0ce4a
flourishcodes <kunle@adeniran.com>
2018-02-09 22:19:06 -0800

    learn the attribute

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -6,13 +6,8 @@
  * @argv: An array of size argc
  * Return: always successful
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-
-	if (argc > 0)
-	{
-		printf("%s\n", argv[0]);
-	}
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - program to print out number of arguments
+ * @argc: size of argv array and number of command line arguments
+ * @argv: An array of size argc
+ * Return: always successful
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
