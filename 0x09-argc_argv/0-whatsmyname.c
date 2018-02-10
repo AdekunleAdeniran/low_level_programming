9c62bfcbb3c477d99a0e3774721373c47c9d114d
flourishcodes <kunle@adeniran.com>
2018-02-09 17:13:06 -0800

    whats my  name

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	int count;
+
+	printf ("%s\n",argv[0]);
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			printf("argv[%d] = %s\n", count, argv[count]);
+		}
+	}
+
+	return 0;
+}
