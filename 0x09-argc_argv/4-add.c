8cf3262e8f5baa9d7a35e3da4e36601b12931668
flourishcodes <kunle@adeniran.com>
2018-02-12 23:21:23 +0000

    update on add

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int count, sum;
+	int count, count1, sum;
 
 	sum = 0;
 	if (argc < 1)
@@ -19,17 +19,16 @@ int main(int argc, char *argv[])
 	}
 	for (count = 1; count < argc; count++)
 	{
-		if (*argv[count] > 47 && *argv[count] < 58)
+		sum += atoi(argv[count]);
+		for (count1 = 0; argv[count][count1] != '\0'; count1++)
 		{
-			sum += atoi(argv[count]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
+			if (!(isdigit(argv[count][count1])))
+			{
+				printf("Error\n");
+				return(0);
+			}
 		}
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
