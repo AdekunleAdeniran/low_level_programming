10df0cae249e5364ecfa5cdf2a72987c35779dcb
flourishcodes <kunle@adeniran.com>
2018-02-13 23:18:36 -0800

    free memory space

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function to free memory
+ * @grid: pointer to grid of int type
+ * @height: int type
+ * Return: clean up memory
+ */
+
+void free_grid(int **grid, int height)
+{
+	int y;
+
+	if (grid == NULL || grid == 0)
+	{
+		return;
+	}
+	for (y = 0; y < height;  y++)
+	{
+		free(grid[y]);
+	}
+	free(grid);
+}
