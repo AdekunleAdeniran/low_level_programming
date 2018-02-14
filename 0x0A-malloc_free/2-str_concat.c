2fac2db00f2a91ab84ffd52633ea7347744783e7
flourishcodes <kunle@adeniran.com>
2018-02-13 19:23:00 -0800

    string concatnation

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,67 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int size1, size2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1) + 1;
+	size2 = _strlen(s2) + 1;
+
+	ptr = malloc((size1 + size2) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	return (ptr);
+}
