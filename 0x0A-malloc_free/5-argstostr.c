ece128b0089f8b5fb5a8701fab73a3e7c5113c9c
flourishcodes <kunle@adeniran.com>
2018-02-14 20:10:36 -0800

    argstostr

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - convert arguments on command line to strings
+ * @ac: int type
+ * @av: pointer to array
+ * Return: arguments as strings
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int size, count, count1, count2 = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; count < ac; count++)
+	{
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		{
+			size += 1;
+		}
+		size += 1;
+	}
+	size += 1;
+
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (count = 0; count < ac; count++)
+	{
+		for (count1 = 0; av[count][count1] != '\0'; count1++)
+		{
+			ptr[count2] = av[count][count1];
+			count2++;
+		}
+		ptr[count2] = '\n';
+		count2++;
+	}
+	ptr[count2] = '\0';
+	return (ptr);
+}
