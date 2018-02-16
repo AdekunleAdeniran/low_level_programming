050b3b39217401b6c151cc04600d6162f2e376bd
flourishcodes <kunle@adeniran.com>
2018-02-15 21:28:08 -0800

    please checkers

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc((nmemb) * sizeof(int));
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 	{
 		return (NULL);
