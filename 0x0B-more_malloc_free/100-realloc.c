0d71bcf85637b25684d5e285f0a81b1324d6ce5a
flourishcodes <kunle@adeniran.com>
2018-02-16 23:15:12 +0000

    advanced 1

---
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *_realloc - reallocate memory size function
+ * @old_size: unsigned int type of old memory size
+ * @new_size: unsigned int type for new memory size
+ * Return:  return pointer to array
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	if (new_size > old_size)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -11,4 +11,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif
