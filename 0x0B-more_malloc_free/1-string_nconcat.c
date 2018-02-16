5ae22372ebb516e2ab1fb7f679de149dfab61150
flourishcodes <kunle@adeniran.com>
2018-02-15 20:00:19 -0800

    update

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _strlen - return length of a string
- *
- * @s: char type
- * Return:  length of string
- */
-
-int _strlen(char *s)
-{
-	int b;
-
-	for (b = 0; s[b] != '\0'; b++)
-	{
-
-	}
-	return (b);
-}
-
-/**
  * string_nconcat - function to concatnate strings with n bytes
  * @s1: destination for concatnation
  * @s2: source of string
@@ -28,37 +10,38 @@ int _strlen(char *s)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int count, count1;
-	int sign = n;
-	char *ptr;
+        int count, count1;
+        int sign = n;
+        char *ptr;
+        int len1, len2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	if (sign >= _strlen(s2))
-	{
-		sign = _strlen(s2);
-		ptr = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-	}
-	else
-	{
-		ptr = malloc(sizeof(char) * (_strlen(s1) + n + 1));
-	}
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-	for (count = 0; count < _strlen(s1); count++)
-	{
-		ptr[count] = s1[count];
-	}
-	for (count1 = 0; count1 < sign; count1++)
-	{
-		ptr[count] = s2[count1];
-		count++;
-	}
-	ptr[count] += '\0';
-	return (ptr);
+        for (len1 = 0; s1[len1] != '\0'; len1++)
+        ;
+        for (len2 = 0; s2[len2] != '\0'; len2++)
+        ;
+
+        if(sign >= len2)
+        {
+               sign = len2;
+               ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+        }
+        else
+                ptr = malloc(sizeof(char) * (len1 + n + 1));
+        if (ptr == NULL)
+                return NULL;
+        for (count = 0; count < len1; count++)
+        {
+                ptr[count] = s1[count];
+        }
+        for(count1 = 0; count1 < sign; count1++)
+        {
+                ptr[count++] = s2[count1];
+        }
+        ptr[count++] = '\0';
+        return (ptr);
 }
