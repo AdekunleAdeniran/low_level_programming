68f5eb44955ffdb7c227d848f018f2ef72bb7efd
flourishcodes <kunle@adeniran.com>
2018-02-15 21:06:41 -0800

    calloc

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - function to allocates memory
+ * @nmemb: unsigned int type
+ * @size: unsigned int type
+ * Return: return pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr;
+	unsigned int count;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc((nmemb) * sizeof(int));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	count = 0;
+	while (count < size)
+	{
+		ptr[count] = nmemb;
+		count++;
+	}
+	ptr[count] = 0;
+	return (ptr);
+}
