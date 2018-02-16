0db70e5468f3548721c414880c4c1b5d4b5d6f1e
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-02-16 13:44:15 -0800

    Update 3-array_range.c

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	ptr = malloc(sizeof(int) * (max - min) + 1);
+	ptr = malloc(sizeof(int) * (max - min + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
