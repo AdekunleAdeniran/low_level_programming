52c71008007133b7e58553eebcc5f20d3aceb261
flourishcodes <kunle@adeniran.com>
2018-02-21 16:12:52 -0800

    updates on betty

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include "function_pointers.h"
+/**
+ * array_iterator - function to iterate through arrays
+ * @array: pointer to array of int type
+ * @size: type of size_t typedef
+ * @action: pointer to function
+ * Return: always successful
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int count;
+
+	if (action == NULL)
+		return;
+	if (size <= 0)
+		return;
+	if (array == NULL)
+		return;
 
 	for (count = 0; count < size; count++)
 	{
