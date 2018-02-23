788a7bfea6db5c7951e741f07134d867f5cc8d25
flourishcodes <kunle@adeniran.com>
2018-02-23 22:41:58 +0000

    Created Variadic functions to print strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - variadic function to print strings
+ * @separator: const char type pointer to string between numbers
+ * @n: const unsigned int type
+ * Return: Always successful
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+	char *test;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		test = va_arg(arg, char*);
+		if (test)
+		{
+			printf("%s", test);
+		}
+		else
+			printf("(nil)");
+		if (separator != NULL && (i < (n - 1)))
+			printf("%s", separator);
+	}
+	va_end(arg);
+	printf("\n");
+}
