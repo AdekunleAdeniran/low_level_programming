647f80f5eda5f532e55363e626308c955baa0e78
flourishcodes <kunle@adeniran.com>
2018-02-23 20:59:47 +0000

    push sum and print

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - variadic function to add number
+ * @n: const unsigned int type
+ * Return: sum of numbers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0;
+	int i;
+	va_list addlist;
+
+	va_start(addlist, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(addlist, unsigned int);
+	}
+	va_end(addlist);
+
+	return (sum);
+}
---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - variadic function to print numbers
+ * @separator: const char type pointer to string between numbers
+ * @n: const unsigned int type
+ * Return: Always successful
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list va;
+	unsigned int i;
+
+	va_start(va, n);
+	for (i = 0; i < n; i++)
+        {
+		printf("%d",  va_arg(va, int));
+		if (separator != NULL && (i < (n-1)))
+                        printf("%s", separator);
+        }
+        printf("\n");
+}
