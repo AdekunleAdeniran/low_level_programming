bd4ba66d0dca5639d6202a4c69e607f30b3a75da
flourishcodes <kunle@adeniran.com>
2018-02-24 13:48:26 -0800

    print all function

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+/**
+ * print_int - function to print ints
+ * @arg: va_list type
+ * Return: always successful
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_char - function to print char
+ * @arg: va_list type
+ * Return: always successful
+ */
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * print_float - function to print floats
+ * @arg: va_list type
+ * Return: always successful
+ */
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * print_string - function to print string
+ * @arg: va_list type
+ * Return: always successful
+ */
+void print_string(va_list arg)
+{
+	char *pr;
+
+	pr = va_arg(arg, char*);
+	if (pr == NULL)
+		printf("(nil");
+	else
+		printf("%s", pr);
+}
+
+/**
+ * print_all - function to print all inputs
+ * @format: const pointer to functionof type char
+ * Return: always successful
+ */
+void print_all(const char * const format, ...)
+{
+	pt types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	unsigned int i, j;
+	va_list arg;
+	char *seperator;
+
+	va_start(arg, format);
+
+	i = 0;
+	seperator = "";
+	while (format && format[i])
+	{
+		j = 0;
+		while (types[j].test != NULL)
+		{
+			if (format[i] == types[j].test[0])
+			{
+				printf("%s", seperator);
+				types[j].printer(arg);
+				seperator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(arg);
+	printf("\n");
+}
