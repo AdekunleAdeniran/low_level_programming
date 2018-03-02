cd7b4170378ad366e6f9dc5595e7f589afd67219
flourishcodes <kunle@adeniran.com>
2018-03-01 23:18:40 -0800

    add node functions

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * *add_node_end - function to add node to start
+ * @head: pointer to pointer of list_t struct type
+ * @str: pointer to string input
+ * Return: Always Successful
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp;
+	list_t *first;
+	unsigned int counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+
+	first = malloc(sizeof(list_t));
+	if (first ==  NULL)
+		return (NULL);
+	first->str  = strdup(str);
+	first->len  = counter;
+
+	if (*head == NULL)
+	{
+		*head = first;
+		first->next = NULL;
+		return (*head);
+	}
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp =  *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = first;
+	first->next = NULL;
+
+	return (*head);
+}
