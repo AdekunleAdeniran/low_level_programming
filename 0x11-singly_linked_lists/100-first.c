c8c2f733bda43205cc131232bc38f3a4a0a67b36
flourishcodes <kunle@adeniran.com>
2018-03-02 17:45:47 -0800

    updated Betty checks

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+/**
+ * myStartupFun - function to run command before main function
+ * Return: always successful
+ */
+void myStartupFun(void) __attribute__ ((constructor));
 
-/* Apply the constructor attribute to myStartupFun() so that it
-    is executed before main() */
-void myStartupFun (void) __attribute__ ((constructor));
-
-void myStartupFun (void)
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
