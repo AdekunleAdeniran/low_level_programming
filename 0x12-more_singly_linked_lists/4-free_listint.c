2f77b07adf9907f183100983a717fbce77594ca4
flourishcodes <kunle@adeniran.com>
2018-03-05 21:59:20 +0000

    updated free

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * free_listint - function to free heap
+ * @head: pointer to struct of listint type
+ * Return: always successful
+ */
 void free_listint(listint_t *head)
 {
 	while (head != NULL)
