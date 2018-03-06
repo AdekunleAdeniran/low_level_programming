59f7377b2933294e7588cd84a707fc17aa65797f
flourishcodes <kunle@adeniran.com>
2018-03-06 08:53:15 -0800

    updated define in list and NULl statement in free

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = NULL;
 
+	if (*head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = temp->next;
 		free(temp);
 	}
-	*head = NULL;
 }
--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <string.h>
