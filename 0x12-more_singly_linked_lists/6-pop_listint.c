d7551eca9db1fb15021fed8dd5b02fe7e53cb6e3
flourishcodes <kunle@adeniran.com>
2018-03-05 23:39:09 -0800

    remove first node

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - function to remove first
+ * @head: pointer to struct node type
+ * Return:  always successful
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *top = NULL;
+	int value = -1;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	top = (*head)->next;
+	value = (*head)->n;
+	*head = top;
+	return (value);
+}
