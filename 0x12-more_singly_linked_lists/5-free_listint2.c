606520a4524d63a91b5e8d80661435d5b776aa37
flourishcodes <kunle@adeniran.com>
2018-03-05 22:59:33 -0800

    free list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function to free memory  used
+ * @head: pointer to pointer of node struct type
+ * Return: always successful
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = NULL;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	*head = NULL;
+}
