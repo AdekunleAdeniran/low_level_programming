edbad6f9a70dc5dd37e62d4a42ab0cfdf276aeef
flourishcodes <kunle@adeniran.com>
2018-03-06 23:42:18 -0800

    delete node

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - functioon to delete node at nth  position
+ * @head: pointer to head
+ * @index: point of node deletion
+ * Return: Always successful
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp = *head;
+	listint_t *temp1;
+	unsigned int count =  0;
+
+	if (*head  == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+	while (count <  (index - 1))
+	{
+		temp = temp->next;
+		count++;
+	}
+	temp1 = temp->next;
+	temp->next  = temp1->next;
+	free(temp1);
+	return (-1);
+}
