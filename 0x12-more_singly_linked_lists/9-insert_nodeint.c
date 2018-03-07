a5b418d42f37a3de63c3ce86fabd64f0a6ce8071
flourishcodes <kunle@adeniran.com>
2018-03-06 22:19:33 -0800

    insert node at nth position

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+/**
+ * listint_len - function to print all elements
+ * @h: listint_t type of node struct
+ * Return: Always successful
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * insert_nodeint_at_index - function to insert node at nth location
+ * @head: pointer to head of struct listint_t type
+ * @idx: unsigned int for location to add node
+ * @n: int type for value of new node
+ * Return: always successful
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *node = NULL;
+	listint_t *temp = NULL;
+	unsigned int count = 0;
+
+	node = malloc(sizeof(listint_t));
+	node->n = n;
+	node->next= NULL;
+	if (*head == NULL)
+		return (NULL);
+	if (idx == 1)
+	{
+		node->next = *head;
+		*head = node;
+		return (*head);
+	}
+	if (idx > 0 && idx < listint_len(*head))
+	{
+		temp = *head;
+		while (count < (idx - 1))
+		{
+			count++;
+			temp = temp->next;
+		}
+		node->next = temp->next;
+		temp->next= node;
+	}
+	return (NULL);
+}
