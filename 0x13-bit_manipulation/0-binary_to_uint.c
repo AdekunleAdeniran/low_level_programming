111ff1993744806fc3bff32f709d8cb3a8419036
flourishcodes <kunle@adeniran.com>
2018-03-08 13:18:46 -0800

    convert binary to int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * binary_to_unit - function to convert binary to unsigned int
+ * @b: pointer to char string input
+ * Return: always successful
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int count = 0;
+	unsigned int check = 1;
+	int x;
+
+	if (b == NULL)
+		return (0);
+	x = 0;
+	while (b[x] != '\0')
+	{
+		if (b[x] != '0' && b[x] != '1')
+		{
+			return (0);
+		}
+		count <<= 1;
+		if (b[x] == '1')
+		{
+			count ^= check;
+		}
+		x++;
+	}
+	return (count);
+}
