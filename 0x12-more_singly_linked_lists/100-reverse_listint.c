98715b6e8e4f7c8d44281c19f21277fb5d682750
flourishcodes <kunle@adeniran.com>
2018-03-07 19:01:15 -0800

    add descriptio

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * *reverse_listint - function to reverse order of nodes
+ * @head:  pointer to head
+ * Return: always successful
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *forward;
