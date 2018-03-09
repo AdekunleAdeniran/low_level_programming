25058393a0e9bdf2987cc66f9ddbee4afae677cb
flourishcodes <kunle@adeniran.com>
2018-03-08 23:40:10 -0800

    add file

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * flip_bits - function to return number of bits
+ * @n: unsigned long int type
+ * @m: unsigned long int type
+ * Return: always successful
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
