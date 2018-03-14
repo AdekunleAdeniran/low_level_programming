546e28fb62f53602dc124a7ce5a677efd499a80a
flourishcodes <kunle@adeniran.com>
2018-03-13 23:35:47 -0700

    test case

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -7,20 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, length;
+	int fd, length, fdwrite;
 
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
 	length = 0;
 	while (*text_content)
 	{
 		length++;
 	}
-
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (fd == -1)
-		return (0);
-	if (write(fd, text_content, length) != length)
+	fdwrite = write(fd, text_content, length);
+	if (fdwrite == -1)
 		return (-1);
 	close(fd);
 	return (1);
