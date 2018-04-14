02b0be1d29a3bc8846230f19a87dda41910f9077
flourishcodes <kunle@adeniran.com>
2018-04-13 21:07:47 -0700

    function to get node at index of list

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function to get the node at index
+ * @head: pointer of dlistint_t type to head of list
+ * @index: unsigned int type for number of index
+ * Return: node at inded if successful, NULL if failed
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 0;
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			temp = head;
+			return (temp);
+		}
+		count++;
+		head = head->next;
+	}
+	return(NULL);
+}
