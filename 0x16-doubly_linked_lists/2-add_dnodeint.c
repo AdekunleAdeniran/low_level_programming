48093bc2871b8b2fbdc568c834d381e07a3fe059
flourishcodes <kunle@adeniran.com>
2018-04-13 22:21:15 -0700

    add node to linked list

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function to add node at the head of a doubly linked list
+ * @head: double pointer of dlistint_t type to head of list
+ * @n: int of const type for data inside list
+ * Return: doubly linked list if successful or NULL if unsuccessful
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (*head);
+	}
+	node->next = *head;
+	*head = node;
+	return (node);
+}
