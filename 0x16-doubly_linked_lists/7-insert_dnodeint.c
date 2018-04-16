39785c0f5979366f6b04884255f0e68bdf76bd06
flourishcodes <kunle@adeniran.com>
2018-04-15 20:52:16 -0700

    edit previous pointers

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,6 +43,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 	}
 	node->next = temp->next;
+	node->prev = temp;
+	temp->next->prev = node;
 	temp->next = node;
 	return (node);
 }
