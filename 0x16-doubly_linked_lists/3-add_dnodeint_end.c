53ba25ded118479c8a342053e45546712639421e
flourishcodes <kunle@adeniran.com>
2018-04-15 17:40:25 -0700

    correction of return NULL

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp = *head;
 
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	node->n = n;
 	node->prev = NULL;
 	node->next = NULL;
