da56bdafbd2c54d113b54a96376ab2d0aa1bb1d2
flourishcodes <kunle@adeniran.com>
2018-04-24 23:52:35 -0700

    Test functions

---
+++ 0x18-stacks_queues_lifo_fifo/mott/README.md
@@ -0,0 +1,14 @@
+## 0x18. C - Stacks, Queues - LIFO, FIFO
+
+**What you should learn from this project**
+       At the end of this project you are expected to be able to explain to
+       anyone, without the help of Google:
+
+* What is LIFO and FIFO
+* What is a stack, and when to use it
+* What is a queue, and when to use it
+* What are the common implementations of stacks and queues
+* What are the most common use cases of stacks and queues
+* What is the proper way to use global variables
+* How to work with git submodules
+
---
+++ 0x18-stacks_queues_lifo_fifo/mott/monty.h
@@ -0,0 +1,57 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void op_push(stack_t **stack, unsigned int line_number);
+
+void op_pall(stack_t **stack, unsigned int line_number);
+
+void op_pint(stack_t **stack, unsigned int line_number);
+
+void op_pop(stack_t **stack, unsigned int line_number);
+
+void op_swap(stack_t **stack, unsigned int line_number);
+
+void op_add(stack_t **stack, unsigned int line_number);
+
+void op_add(stack_t **stack, unsigned int line_number);
+
+#endif
