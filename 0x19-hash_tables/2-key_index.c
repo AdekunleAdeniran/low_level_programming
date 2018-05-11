349f3373fe99b9b4804136739851b7971b45a03b
flourishcodes <kunle@adeniran.com>
2018-05-11 10:15:18 -0700

    Hash function to get index of a key

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - C function to find index of a key
+ * @key: pointer to key
+ * @size: size of the array of the hash table
+ * Return: index of key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash;
+
+	hash = hash_djb2(key);
+	return (hash % size);
+}
