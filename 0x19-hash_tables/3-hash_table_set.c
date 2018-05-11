257db027c3dd727234d04587b57dd1db4831b173
flourishcodes <32856210+flourishcodes@users.noreply.github.com>
2018-05-11 16:32:15 -0700

    Update 3-hash_table_set.c

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -23,7 +23,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[index]->key,key) == 0)
 		{
+			free(ht->array[index]->value);
+			free(new)
 			ht->array[index]->value = strdup(value);
+			return(1);
 		}
 		ht->array[index] = ht->array[index]->next;
 	}
