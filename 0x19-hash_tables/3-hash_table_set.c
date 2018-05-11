dd2ded15fd00c383e358b4064d6cc21665a2ab1f
flourishcodes <kunle@adeniran.com>
2018-05-11 16:48:51 -0700

    Has function to add an element to a hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,37 +9,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *new;
+	hash_node_t *new, *temp;
 
 	if (!ht || !ht->array || !key || !value || strlen(key) == 0)
 	{
 		return (0);
 	}
+	if (strcmp("", key) == 0)
+		return (0);
+
 	index = key_index((const unsigned char *) key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	if (ht->array[index] != NULL)
+	temp = ht->array[index];
+	while (temp != NULL)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
+		if (strcmp(temp->key, key) == 0)
 		{
-			free(ht->array[index]->value);
+			free(temp->value);
 			free(new);
-			ht->array[index]->value = strdup(value);
+			temp->value = strdup(value);
 			return (1);
 		}
-		ht->array[index] = ht->array[index]->next;
+		temp = temp->next;
 	}
 	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		return (0);
-	}
 	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		return (0);
-	}
 	new->next = ht->array[index];
 	ht->array[index] = new;
 	return (1);
