50c01d721a0425d0e9f7301ed6d7f90c565902d7
flourishcodes <kunle@adeniran.com>
2018-05-11 10:26:04 -0700

    Hash function to get index of a key

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -9,6 +9,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash;
 
-	hash = hash_djb2(key);
-	return (hash % size);
+	hash = hash_djb2(key) % size;
+	return (hash);
 }
