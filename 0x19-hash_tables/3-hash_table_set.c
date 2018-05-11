73fe536664912285d6c806d8fcb53a06193d3293
flourishcodes <kunle@adeniran.com>
2018-05-11 16:35:19 -0700

    Has function to add an element to a hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(ht->array[index]->key,key) == 0)
 		{
 			free(ht->array[index]->value);
-			free(new)
+			free(new);
 			ht->array[index]->value = strdup(value);
 			return(1);
 		}
