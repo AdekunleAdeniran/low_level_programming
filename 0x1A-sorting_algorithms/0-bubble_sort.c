90594749a5313968d0e17727367d26be8d6ee709
flourishcodes <kunle@adeniran.com>
2018-05-27 21:40:05 -0700

    C Bubble sort function for an array

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - C function that uses uses the Bubble sort method in an array
+ * @array: array of integers to sort
+ * @size: the size of the array to be sorted
+ * Return: sorted array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t check = size;
+	size_t i, temp;
+
+	while (check > 0)
+	{
+		for (i = 1; array && i < size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = temp;
+				print_array(array, size);
+			}
+		}
+		check--;
+	}
+}
