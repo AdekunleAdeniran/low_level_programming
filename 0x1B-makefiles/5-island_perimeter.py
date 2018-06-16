d0f02673c0f6c6766f25fba490672197827aac97
flourishcodes <kunle@adeniran.com>
2018-06-15 22:05:38 -0700

    Python Function to solve the Island perimeter challenge

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -3,13 +3,17 @@
 
 def island_perimeter(grid):
     """Python function to return the perimeter of an island"""
-    l, b = 0, 1
+
+    p = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                if grid[x][y + 1] == 0:
-                    l += 1
-                elif grid[x][y + 1] == 1:
-                    b += 1
-    p = 2 * (l + b)
+                if x + 1 == len(grid) or grid[x + 1][y] == 0:
+                    p += 1
+                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
+                    p += 1
+                if x - 1 == -1 or grid[x - 1][y] == 0:
+                    p += 1
+                if y - 1 == -1 or grid[x][y - 1] == 0:
+                    p += 1
     return(p)
