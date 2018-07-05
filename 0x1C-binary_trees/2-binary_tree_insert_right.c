60cef5c481ee19554b0c5aa367c46c7387c82e18
flourishcodes <kunle@adeniran.com>
2018-07-05 00:57:29 -0700

    C function to add node to right side of binary tree

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -7,11 +7,11 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
 
 	if (parent == NULL)
 		return (NULL);
-
+	newNode  = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = value;
