a0ccc2a064e25ee11921eb6694fe23f312711721
flourishcodes <kunle@adeniran.com>
2018-07-05 16:36:59 -0700

    Rewrote depth function for first and second

--- 0x1C-binary_trees/100-binary_trees_ancestor.c
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -1,4 +1,20 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_depth - calculate levels from parent
+ * @node: node
+ * Return: levels node is from parent
+ */
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node && (node->parent))
+		return (1 + binary_tree_depth(node->parent));
+
+	return (0);
+}
 /**
  * binary_trees_ancestor - C function to find common ancestor of two nodes
  * @first: pointer to first node
@@ -12,19 +28,25 @@ binary_tree_t *binary_trees_ancestor(
 
 	if (!first || !second)
 		return (NULL);
-
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
 	if (first->parent == second->parent)
 		return (first->parent);
-	if (second->parent == first)
-		return ((binary_tree_t *)first);
+
 	left = binary_trees_ancestor(first->parent, second);
 	right = binary_trees_ancestor(second->parent, first);
 
+	if (binary_tree_depth(first) > binary_tree_depth(second))
+		return (left);
+	else if (binary_tree_depth(second) > binary_tree_depth(first))
+		return (right);
+	return (binary_trees_ancestor(first->parent, second->parent));
+/*
 	if (left != NULL && right != NULL)
 		return (first->parent);
 	else if (left == NULL && right == NULL)
 		return (NULL);
 	else
 		return (left == NULL ? right : left);
-	return (NULL);
+*/
 }
