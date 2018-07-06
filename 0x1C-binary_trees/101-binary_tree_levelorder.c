c23411fff3f7cefcfef26d62b67c4579573884b4
flourishcodes <kunle@adeniran.com>
2018-07-05 19:20:55 -0700

    C function to print in level order

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -1,5 +1,57 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_l, height_r;
+
+	if (tree == NULL)
+		return (0);
+
+	height_l = binary_tree_height(tree->left);
+	height_r = binary_tree_height(tree->right);
+	if (height_l > height_r)
+		return (height_l + 1);
+	return (height_r + 1);
+}
+/**
+ * printGivenLevel - C Function to print values at all levels of binary tree
+ * @tree: pointer to tree/nodes whose values will be printed
+ * @level: level at which values will be printed
+ * Return: printed values
+ */
+void printGivenLevel(const binary_tree_t *tree, int level)
+{
+	if (tree == NULL)
+		return;
+	if (level == 1)
+		printf("%d\n", tree->n);
+	else if (level > 1)
+	{
+		printGivenLevel(tree->left, level - 1);
+		printGivenLevel(tree->right, level - 1);
+	}
+}
+/**
+ * printLevelOrder - C function to print in level order
+ * @tree: pointer to node to be printed
+ * Return: printed data
+ */
+void printLevelOrder(const binary_tree_t *tree)
+{
+	int height = binary_tree_height(tree);
+	int count;
+
+	for (count = 1; count <= height; count++)
+		printGivenLevel(tree, count);
+
+}
+/**
  * binary_tree_levelorder - C function to print in level order
  * @tree: pointer to node to be printed
  * @func: function pointer
@@ -8,16 +60,6 @@
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		return (NULL);
-
-	if (tree->parent == NULL)
-		func(tree->n);
-	if (tree->left != NULL)
-		func(tree->left->n);
-	if (tree->right != NULL)
-		func(tree->right->n);
-	if (tree->left != NULL)
-		binary_tree_levelorder(tree->left, func);
-	if (tree->right != NULL)
-		binary_tree_levelorder(tree->right, func);
+		return;
+	printLevelOrder(tree);
 }
